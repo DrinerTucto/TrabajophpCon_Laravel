3dd8514a955b26b8da3092727eb4c3d60dda7dad
DrinerTucto <1304745@senati.pe>
2022-11-09 12:37:29 -0500

    mas cambien

--- resources/views/reservacion/index.blade.php
+++ resources/views/reservacion/index.blade.php
@@ -1,9 +1,6 @@
+<html>
+<body>
 
-
-@if(Session::has('mensaje'))
-{{Session::get('mensaje')}}
-
-@endif
 
 <style>
 
@@ -12,7 +9,7 @@ img{
     height: 100px;
 }
 body{
-    display: flex;
+    display: inline-block;
     align-items: center;
     justify-content: center;
     min-height: 100vh;
@@ -23,30 +20,68 @@ body{
     background-size: cover;
 }
 .contenedor{
+    
+    text-align: center;
+    margin-top: 20px;
     border: solid 2px white;
+    margin-left: 30px;
 
 }
+
 thead{
     background-color: brown;
 }
 th{
-    padding: 20px 15px;
+    padding: 20px 75px;
     font-weight: 700;
 
 }
 td{
-    padding: 15px;
-    border-bottom:solid 2px white;
+    padding: 15px 30px;
+    border-top:solid 2px white;
 
+
+
+
+      
 }
-.holas{
+li{
+    display: inline-block;
+    text-align: center;
+    justify-content: center;
+}
+header{
     display: flex;
+    justify-content: space-between;
 }
-
+.logo{
+width: 50px;
+height: 50px;
+}
+.exitoso{
+    color: rgb(0, 0, 0);
+    font-family: Georgia, 'Times New Roman', Times, serif;
+    font-size: 30px;
+}
 </style>
 
+  <header>
+  
+<div class="A">
+    <img class="logo" src="https://cdn-icons-png.flaticon.com/512/984/984284.png" alt="">
+    <span>ViajaConDriner</span>
+</div>
+<nav class="menu">
+    <li><a href="{{url('reservacion/reserva')}}">NUEVA RESERVACION</a></li>
+    <li><a href="#">Contactos</a></li>
+</nav>
+</header>
+<div class="exitoso">
+@if(Session::has('mensaje'))
+{{Session::get('mensaje')}}
 
-
+@endif
+</div>
 <div class="contenedor">
     <h1>Reservacion de viajes</h1>
 
@@ -89,6 +124,8 @@ td{
     </tbody>        
 
 </table>    
-<a href="{{url('reservacion/reserva')}}">NUEVA RESERVACION</a>
 
 </div>
+
+</body>
+</html>
