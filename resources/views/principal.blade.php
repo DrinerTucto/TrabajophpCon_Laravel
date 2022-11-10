608932bcfe0df4f029d0b0907cb885cd77f73608
DrinerTucto <1304745@senati.pe>
2022-11-10 13:43:30 -0500

    mas avanse de diseño

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -39,10 +39,16 @@ h4{
        
 
 }
-.abajo{
+.abajo1{
     margin-left: 20px;
     width:70px;
     height: 70px;
+    
+}
+.abajo{
+    margin-left: 20px;
+    width:400px;
+    height: 370px;
     
 }
 .parrafo img{
@@ -74,7 +80,7 @@ p{
 
 <div class="t">
     <a href="https://cdn-icons-png.flaticon.com/512/4574/4574365.png ">
-    <img class="abajo" src="https://cdn-icons-png.flaticon.com/512/4151/4151471.png "> </a>
+    <img class="abajo1" src="https://cdn-icons-png.flaticon.com/512/4151/4151471.png "> </a>
 </div>
 </div>
 <div class="pa">
@@ -84,6 +90,7 @@ p{
     <img class="abajo" src="http://blog.redbus.pe/wp-content/uploads/2020/06/peru-3978148_1920.jpg" alt="">
 
     <p>Pocos lugares hay en el Perú que despiertan tanto interés como el espectáculo que representa Machu Picchu para todo el que tenga la fortuna de ir alguna vez en la vida a esta belleza de sitio. Este es uno de esos lugares que siempre debe estar incluido en cualquier lista de destinos preferidos del país andino, así que aquí tenía que estar. Esta belleza de santuario histórico se ha convertido en un verdadero ícono para todo peruano del mundo, ya que el orgullo que despierta en todos los ciudadanos de esta nación simplemente no puede ser descrito con palabras. Es por todo el interés que ha generado por parte del resto del planeta que se declaró el santuario como Patrimonio Histórico de la Humanidad en la década de 1980 y no es para menos que así haya resultado ser, de hecho mencionar al mencionar al Perú sin duda alguna nos trae a la mente este hermosa maravilla del mundo que sin duda sigue siendo uno de los principales lugares turísticos del perú . Una de las principales razones por las que ha ganado tanta importancia es por la arqueología tan compleja que envuelve a este lugar, pero a eso hay que sumarle todos los ecosistemas que hacen vida en el sitio. Eso sin mencionar la diversidad en flora y fauna que se puede hallar aquí y que ha dejado impactados a los expertos del mundo entero, quienes se han dejado maravillar por el espectáculo natural que Machu Picchu les ofrece.</p>
+<br>
 </div>
 
 
@@ -101,59 +108,63 @@ p{
     <h1>Líneas de nazca
     </h1></div> <div class="parrafo"> <img class="abajo"  src="https://reisexpert.b-cdn.net/wp-content/uploads/2018/10/Cuzco-Peru.jpg" alt="">
     <p>Una de las razones por las que ha llamado tanto la atención del turismo latino es que no han podido ser precisados los nombres de sus autores ni con qué fin las hicieron. Lo que sí se ha descubierto es que las líneas pueden datar desde 200 años A.C., pero aún continúan realizándose todo tipo de estudios a finde determinar el tiempo que tiene talladas con una mayor exactitud que la que ya se posee. Algunas de las teorías más alocadas sobre el objetivo con el cual fueron hechas las líneas es que el o los autores querían que se pareciesen a una especie de calendario astronómico que calcule todos y cada uno de los movimientos realizados por los cuerpos celestes presentes en el espacio. Sigue siendo una teoría que muchos consideran poco confiable debido a la poca evidencia que hay al respecto.</p>
- </div>
+    <br><br><br><br><br><br>
+</div>
 <div>
     <h1>Parque Nacional Huascarán
     </h1></div>
-     <div class="parrafo"> <img class="abajo"  src="https://reisexpert.b-cdn.net/wp-content/uploads/2018/10/Cuzco-Peru.jpg" alt="">
+     <div class="parrafo"><img class="abajo"  src="https://reisexpert.b-cdn.net/wp-content/uploads/2018/10/Cuzco-Peru.jpg" alt="">
     <p> Su ubica en el Departamento de Ancash, aunque se dice que el modo más rápido de llegada es yéndose por Huaraz, un poblado cercano. Tal y como ha sido el caso de una gran cantidad de monumentos y poblaciones mencionadas en esta lista, Este parque nacional también ha pasado a engrosar el grupo de lugares que fueron declarados como Patrimonios de la Humanidad por parte de la UNESCO, cosa que es un gran orgullo para todos los peruanos que ven como muchos de sus más representativos puntos son reconocidos por todo el mundo. A todo lo anterior hay que añadirle que posee uno de los picos de mayor altura d la nación entera, el cual es subido por muchos alpinistas que se esfuerzan por llegar a la cima siempre que se les da la oportunidad.</p>
+    <br><br><br><br><br><br>
+
 </div>
 <div>
     <h1>Arequipa</h1></div> <div class="parrafo"> <img class="abajo"  src="https://reisexpert.b-cdn.net/wp-content/uploads/2018/10/Cuzco-Peru.jpg" alt="">
     <p>Esta hermosísima ciudad, también conocida como la Ciudad Blanca, ha sido otro de los tantos puntos preferidos por los turistas debido a la arquitectura colonial que ha conservado a pesar del modernismo en el que ha entrado el país en los últimos años. Tal y como es el caso de Lima, también posee un hermoso Centro Histórico que es digno de admirar por todo el que decida ir a la ciudad. Si hay algo que destacar de este lugar es el que ha sido declarado como Patrimonio de la Humanidad, así que hay un gran trozo de historia que cuidar aquí. Al Centro Histórico hay que sumarle otros lugares que también deberían ser del interés de los visitantes como lo son la Plaza de Armas y el Convento Santa Catalina. Muchos dicen que sus plazas y estrechas calles recuerdan mucho a la arquitectura de Andalucía, España. Todo lo que esta hermosa ciudad tiene para brindar no se encuentra en muchos lugares del mundo, así que hay que aprovecharla cada vez que se vaya al Perú.</p>
+    <br><br><br><br>
+
 </div>
 <div>
-    <h1>Piura</h1></div>
+    <h1>Piura</h1></div> <div class="parrafo"><img class="abajo" src="https://reisexpert.b-cdn.net/wp-content/uploads/2018/10/Cuzco-Peru.jpg" alt="">
 <p>Piura es el lugar más indicado para encontrar las mejores de todo el país latino. Este es el lugar preferido para todos los que deseen practicar todo tipo de deportes acuáticos que no serían posibles de llevar a cabo en otros lugares de la nación. Tal es el caso del surf, el cual ha ganado mucha popularidad en los últimos años gracias a lo aptas que están algunas de estas playas para esta actividad. Si es esto lo que se va a hacer en la playa, se recomienda ir a Cabo Blanco, Colán o Máncora. Si lo que se quiere es buscar la playa ideal para el turismo, se recomienda ir a Colán, ya que esta es la que posee la mayor cantidad de complejos hoteleros, bares y restaurantes que más pueden proveer a los visitantes de lo que les hace falta para pasar unas vacaciones inolvidables. En caso de que lo que se busque sea disfrutar de los mejores centros nocturnos para la juventud, Máncora es la mejor opción de las 3 ya nombradas.</p>
+</div>    <br><br><br><br>
 
 <div>
     <h1>Valle Sagrado de los Incas
     </h1></div> <div class="parrafo"> <img  class="abajo" src="https://reisexpert.b-cdn.net/wp-content/uploads/2018/10/Cuzco-Peru.jpg" alt="">
 <p>La magia del Valle Sagrado radica en que conserva mucho de la arqueología y color que poseen mucho de los poblados habitados por etnias indígenas, lo cual se ha mantenido aún con vida a pesar del tiempo transcurrido. Uno de los puntos que más suele llamar la atención de los amantes de la historia que visitan el Perú es la población de Pisac, la cual siempre ha tenido mucha fama gracias a sus tejedoras y gran valor arqueológico. Algo muy interesante es que estas zonas siguen siendo habitadas por la descendencia del pueblo Inca y se puede decir que las modificaciones que han sufrido han sido nulas en comparación con otros lugares. Además de que las edificaciones se han mantenido a pesar de los siglos transcurridos, son los descendientes directos de los pueblos indígenas de la época los que hacen vida en estos lugares, lo que hace aún más llamativo a este poblado.</p>
-</div>
+</div>    <br><br><br><br><br><br>
+
 <div>
     <h1>Vinicunca, la montaña de 7 colores
     </h1></div> <div class="parrafo"> <img class="abajo"  src="https://reisexpert.b-cdn.net/wp-content/uploads/2018/10/Cuzco-Peru.jpg" alt="">
 <p>la Montaña Vinicunca. Resulta ser que esta obra de arte de la naturaleza tiene, por lo menos, 7 tonos distintos y resalta entre todas las demás que puedan existir en el Perú y el resto del mundo. Sin duda alguna no podemos hablar de lugares turísticos del Perú sin mencionar este maravilla de la nación andina, pero también lo es de todo el mundo porque no hay nada parecido que se haya visto jamás, así que no se puede dejar de visitarla cuando se va de viaje de placer a este país. La razón primordial por la que tiene ese aspecto tan particular es que se compone de una serie de minerales cuya mezcla hace que las tonalidades adquieran este aspecto, cosa que ha enloquecido de curiosidad a todo el que tiene la fortuna de ver la montaña, aunque sea una vez en la vida. Hay varios que se aventuran a legar al pico, pero no es una tarea sencilla. Se requieren de años y años de experiencia para llegar a semejante altura y no verse afectado físicamente durante el recorrido.</p>
-</div>
+</div>    <br><br><br><br><br><br>
+
 <div>
     <h1>El Lago Titicaca
-    </h1></div>
+    </h1></div> <div class="parrafo"><img class="abajo" src="https://reisexpert.b-cdn.net/wp-content/uploads/2018/10/Cuzco-Peru.jpg" alt="">
     <p> Este es uno de los lagos navegables más altos del planeta, así que no puede dejarse por fuera ir al a la nación andina. Lo que más ha atraído la atención de los turistas hacia él han sido sus inmensas riquezas naturales y las 3 preciosas islas de las que se compone. En todas se puede percibir la continuidad de la tradición y rasgos culturales del Perú precolombino, cosa que no percibe con fuerza en muchos lugares del continente. En el caso específico de la Isla Flotante de Uros, es muy importante destacar que se puede acceder a ella por un recorrido en lancha de un tiempo aproximado de media hora. Se cree que su construcción se llevó a cabo a base de totora y que su existencia representa la continuidad de una de las civilizaciones de mayor antigüedad del planeta. Quienes habitan las islas han podido vivir a base de la pesca, la venta de artesanías y todo el aparataje turístico que este tipo de lugares representan.</p>
+</div>    <br><br><br><br><br><br>
 
 <div>
     <h1>Parque Nacional de Manu, Madre de Dios
     </h1></div> <div class="parrafo"> <img class="abajo"  src="https://reisexpert.b-cdn.net/wp-content/uploads/2018/10/Cuzco-Peru.jpg" alt="">
     <p>El Parque Nacional de Manu es un área natural protegida con la mayor diversidad de reptiles, anfibios, mariposas diurnas y aves de todo el planeta. Es Patrimonio Natural de la Humanidad. Es una de las mayores reservas de biósfera en el Mundo. Y fue creado con el fin de catalogar, preservar y dará conocer a la humanidad la herencia natural y cultural de la selva amazónica.
-</div>
+</div>    
         En la Reserva Nacional del Manu se encuentra la mayor diversidad biológica del planeta. Además, es el hogar de numerosos grupos étnicos: Kugapakori, Nahua, Matsiguenga, Harakmbut, Yine y algunos grupos étnicos que viven en aislamiento voluntario.</p>
 
-<div>
+<div>    <br><br><br><br><br><br>
+
     <h1>Valle Sagrado de los Incas, Cusco</h1></div> <div class="parrafo"> <img class="abajo"  src="https://reisexpert.b-cdn.net/wp-content/uploads/2018/10/Cuzco-Peru.jpg" alt="">
     <p>Es un extenso valle de clima templado que se extiende a ambos lados del río Urubamba. Se caracteriza por su clima templado y por sus fértiles tierras dónde se produce el mejor maíz del mundo y muchas especies de hortalizas.
 
         En las inmediaciones del Valle Sagrado de los Incas están ubicados los principales complejos arqueológicos de la ciudad de Cusco: Pisac, Ollantaytambo, Moray, Salineras y Chinchero. Asimismo, aquí están ubicados los pueblos tradicionales más importantes de Cusco (Pisac, Maras, Ollantaytambo y Chinchero), donde aún se conservan y practican el legado cultural de los incas.</p>
 </div>
-<div>
+<div>    <br><br><br><br><br><br>
+
     <h1>Camino Inca, Cusco
     </h1></div> <div class="parrafo"> <img  class="abajo" src="https://reisexpert.b-cdn.net/wp-content/uploads/2018/10/Cuzco-Peru.jpg" alt="">
     <p>El Camino Inca a Machu Picchu es una de las rutas de trekking más hermosas y concurridas de América y del mundo. Forma parte de la red del Qhapaq Ñan, la antigua red vial de los incas que medía más de 20000 km y se extendía a lo largo y ancho del Tahuantinsuyo. Actualmente, la ruta de trekking empieza en el kilómetro 82, de la línea férrea Cusco – Machu Picchu, y tiene un recorrido de 43 km que se recorren en 4 días.</p>
 </div>
-<div> <div class="parrafo"> <img class="abajo"  src="https://reisexpert.b-cdn.net/wp-content/uploads/2018/10/Cuzco-Peru.jpg" alt="">
-    <h1>Ciudadela de Chan Chan, Trujillo
-    </h1></div>
-<p>Chan Chan es la ciudad de adobe más grande de América y la segunda del mundo. Fue construido por los chimúes, cultura pre inca que se desarrolló en la costa norte de Perú, alrededor de los años 900 y 1300 d.C. Está ubicado en el valle de Moche, entre el balneario de Huanchaco y la ciudad de Trujillo, en los distritos de Trujillo y Huanchaco.
-
-    El complejo arqueológico de Chan Chan abarca 20 hectáreas de extensión. Y está formado por diez pequeñas ciudades amuralladas, en cuyas paredes resaltan los decorados en alto relieve de figuras geométricas, peces y aves. Chan Chan fue la capital administrativa de la cultura Chimú que llegó a acoger entre veinte mil y treinta mil habitantes.</p>
-
-</div>
+<div>    <br><br><br><br><br><br><br><br>
